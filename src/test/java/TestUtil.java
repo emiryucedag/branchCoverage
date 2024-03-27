6c08107d843925338c3d127ecfa2be9c43e0312d
emiryucedag <emiryucedag@hotmail.com>
2024-03-27 20:46:12 +0300

    Test Util has done by %100 coverage.

--- src/test/java/TestUtil.java
+++ src/test/java/TestUtil.java
@@ -2,6 +2,9 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,4 +16,20 @@ public class TestUtil {
 
   @Test
   public void example() { assertTrue(true); }
+
+  @Test
+  public void testLengthIsOne() {assertFalse(c.compute(1));}
+  @Test
+  public void testLengthIsEven() {assertFalse(c.compute(1,2,3,4));}
+  @Test(expected = RuntimeException.class)
+  public void testZero() {
+    c.compute(1, 0,3);
+  }
+  @Test
+  public void testSumDivisionTrue() {assertTrue(c.compute(1,2,3));}
+
+  @Test
+  public void testSumDivisionFalse() {assertFalse(c.compute(3,7,13));}
+
+
 }
